f2501254b5e250ed567db748ce608d79e697acdd
MikuXvDev <39mkui@gmail.com>
2025-06-01 11:25:49 +0300

    добавление перевода

--- app/handlers/ask.py
+++ app/handlers/ask.py
@@ -3,6 +3,7 @@ from aiogram.types import Message
 from aiogram import Router, Bot, F
 from aiogram.filters import Command
 from aiogram.enums.chat_type import ChatType
+from aiogram_i18n import I18nContext
 
 from .mita import mita
 
@@ -14,8 +15,8 @@ last_bot_message = {}
 
 
 @ask_router.message(Command("ask"), F.chat.type.in_([ChatType.GROUP, ChatType.SUPERGROUP]))
-async def ask(message: Message, bot: Bot) -> None:
-    await message.reply("<b>❤️ | Я тебе уже пишу!</b>")
+async def ask(message: Message, i18n: I18nContext, bot: Bot) -> None:
+    await message.reply(text=i18n.get("waiting_for_message_neural"))
     user_id = message.from_user.id
     bot_response = await mita(message, bot)
 
@@ -30,5 +31,3 @@ async def handle_reply_to_bot(message: Message, bot: Bot) -> None:
     if user_id in last_bot_message and message.reply_to_message.message_id == last_bot_message[user_id]:
         bot_response = await mita(message, bot)
         last_bot_message[user_id] = bot_response.message_id
-    else:
-        print("Not last message")
