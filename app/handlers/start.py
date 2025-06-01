7ed9edc10614acebc6cc30bfea1fed88a97772e7
MikuXvDev <39mkui@gmail.com>
2025-06-01 11:18:07 +0300

    Добавление стартового видео

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,13 +1,13 @@
 from aiogram import Router
 from aiogram.types import Message
+from aiogram.types.input_file import URLInputFile
 from aiogram_i18n import I18nContext
 
 from ..database.requests import DatabaseManager
 from ..services.conditions import conditions_accept
-
+from ...config_reader import config
 
 start_router = Router()
-
 
 async def start(message: Message, i18n: I18nContext) -> None:
     db = DatabaseManager(message.from_user.id)
@@ -17,6 +17,7 @@ async def start(message: Message, i18n: I18nContext) -> None:
         await conditions_accept(message, i18n)
         return
 
-    await message.reply(
-        text=i18n.get("hello")
+    await message.reply_video(
+        video=(URLInputFile(config.start_video_url.get_secret_value())),
+        caption=i18n.get("hello")
     )
