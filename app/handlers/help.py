11ca99b476c1e312c978405329240c04d05c1666
MikuXvDev <39mkui@gmail.com>
2025-06-02 14:35:19 +0300

    Добавлена картинке в help

--- app/handlers/help.py
+++ app/handlers/help.py
@@ -2,6 +2,9 @@ from aiogram import Router
 from aiogram.types import Message
 from aiogram_i18n import I18nContext
 from aiogram.filters.command import Command
+from aiogram.types.input_file import URLInputFile
+
+from ...config_reader import config
 
 
 help_router = Router()
@@ -9,6 +12,7 @@ help_router = Router()
 
 @help_router.message(Command('help'))
 async def help(message: Message, i18n: I18nContext) -> None:
-    await message.reply(
-        text=i18n.get("help_message")
+    await message.reply_photo(
+        photo=URLInputFile(config.help_pic_url.get_secret_value()),
+        caption=i18n.get("help_message")
     )
