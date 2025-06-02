0b6ac5084d369c2fee108ec4c2a0fc55d7b053d5
MikuXvDev <39mkui@gmail.com>
2025-06-02 14:15:13 +0300

    Исправление путей

--- app/services/voice_person.py
+++ app/services/voice_person.py
@@ -1,15 +1,15 @@
 import os
 import json
 import aiofiles
+from pathlib import Path
 from types import SimpleNamespace
-
 
 class VoicePerson:
     def __init__(self, person) -> None:
         self.person = person
 
     async def get_params(self) -> SimpleNamespace:
-        async with aiofiles.open(f"{os.getcwd()}/AIO-MITA/persons.json", mode='r', encoding='utf-8') as f:
+        async with aiofiles.open(Path(__file__).resolve().parent.parent.parent / "persons.json", mode='r', encoding='utf-8') as f:
             persons = json.loads(await f.read())
 
         if persons.get(self.person) is None:
