d9319fbb984b04584c7913f9b24cfecc71a76346
MikuXvDev <39mkui@gmail.com>
2025-06-09 21:52:21 +0300

    Добавление активации подписки пользователю

---
+++ app/handlers/configs/subscribe.py
@@ -0,0 +1,31 @@
+from aiogram import Router, F
+from aiogram.types import Message
+from aiogram.filters.command import Command
+
+from ...database.requests import DatabaseManager
+
+subscribe_router = Router()
+
+
+@subscribe_router.message(Command('subscribe'), F.from_user.id == 6506201559)
+async def subscribe(message: Message) -> None:
+    text = message.text.split(maxsplit=2)
+
+    db = DatabaseManager(message.from_user.id)
+
+    if len(text) != 3:
+        await message.reply("не правильно используешь команду")
+        return
+
+    if text[2].lower() not in ('-', '+'):
+        await message.reply('Не правильн! + или - !')
+        return
+
+
+    try:
+        await db.set_subscribe(1 if text[2] == '+' else 0) # Вызываем blocker, но не используем его возвращаемое значение.
+    except Exception as e:
+        await message.reply(text=f"{e}")
+        return
+
+    await message.reply(text=f"Подписка была активирована")
