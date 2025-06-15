ca1b868a95850528a00c625daf88193097991f9e
MikuXvDev <39mkui@gmail.com>
2025-06-15 14:00:50 +0300

    Новая коллекция Subscribe

--- app/database/db.py
+++ app/database/db.py
@@ -1,13 +1,9 @@
-# NOTE: Код ChatGPT, который нужно переписать
 from typing import Optional, List
-
-
 from bson import ObjectId
 from pydantic import BaseModel, Field
 from pydantic import GetJsonSchemaHandler
 from pydantic.json_schema import JsonSchemaValue
 from motor.motor_asyncio import AsyncIOMotorClient
-
 
 from ...config_reader import config
 
@@ -43,11 +39,9 @@ class UserModel(BaseModel):
     voice_mode: bool = False
     lang: str = "null"
     voice_engine: int = 1
-    subscribe: int = 0
 
     class Config:
         populate_by_name = True
-
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
         allow_population_by_field_name = True
@@ -67,18 +61,34 @@ class StatistikModel(BaseModel):
 
     class Config:
         populate_by_name = True
-
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
         allow_population_by_field_name = True
 
-# Подключение к базе
+
+class SubscribeModel(BaseModel):
+    id: Optional[PyObjectId] = Field(default_factory=PyObjectId, alias="_id")
+    tg_id: int = Field(..., description="Ссылка на User.tg_id")
+    subscribe: int = 0  # 0 - нет подписки, 1 - активна
+    period: Optional[str] = None
+    free_voice: int = 0  # Сколько уже потратил
+    left_free_voice: int = 30  # Сколько всего доступно (можно менять из БД)
+
+
+    class Config:
+        populate_by_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        allow_population_by_field_name = True
+
+
 client = AsyncIOMotorClient(config.mongo_db.get_secret_value())
 db = client[config.mongo_name.get_secret_value()]
 
-# Коллекции
 users_collection = db['users']
 statistik_collection = db['statistiks']
+subscribe_collection = db['subscribes']
+
 
 async def async_main():
     pass
