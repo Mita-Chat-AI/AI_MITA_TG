fba03d9422e9f4b8fff22354aed6ef84076c710e
MikuXvDev <39mkui@gmail.com>
2025-06-15 14:01:11 +0300

    tik_tak_toe_router

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -17,3 +17,4 @@ from .mailing import mailing_router
 from ..services.conv_timer import conv_timer_router
 from .configs.set_voice_engine import set_voice_engine_router
 from .configs.subscribe import subscribe_router
+from .tik_tak_toe import tik_tak_toe_router
